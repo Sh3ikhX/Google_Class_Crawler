167b5747988125d71e7f1fa14c41d94245d9f544
SheikhX <my7h5542@gmail.com>
2021-03-29 12:26:27 +0500

    Working File metadata

--- file_data.py
+++ file_data.py
@@ -33,16 +33,17 @@ def main():
     service = build('drive', 'v3', credentials=creds)
 
     # Call the Drive v3 API
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    results = service.files().list(pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    results1 = service.files().get(fileId='1qFdF1M-YYMcEz2l2JA7k0g1uhIjuktGT',fields='videoMediaMetadata').execute()
+    print(results1)
     items = results.get('files', [])
 
-    if not items:
-        print('No files found.')
-    else:
-        print('Files:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+    # if not items:
+    #     print('No files found.')
+    # else:
+    #     print('Files:')
+    #     for item in items:
+    #         print(u'{0} ({1})'.format(item['name'], item['id']))
 
 if __name__ == '__main__':
     main()
