1476552de959a9461ea0de54d8e33c12c7270b34
SheikhX <my7h5542@gmail.com>
2021-04-02 17:26:24 +0500

    Working DB And added date field to sort in date

--- db.py
+++ db.py
@@ -12,28 +12,4 @@ except Exception as e:
 
 print(mydb)
 mycursor = mydb.cursor()
-sql = "SELECT * FROM courses"
-mycursor.execute(sql)
-myresult = mycursor.fetchall()
-# for x in myresult:
-#   if(x[3]==0 orx[4]==0 or x[5]==0)
 
-
-#
-# sql = "INSERT INTO courses (coursename, course_id) VALUES (%s, %s)"
-# val = (str("course['name']"), "course['id']")
-# mycursor.execute(sql, val)
-#
-# mycursor.execute("SELECT * FROM courses")
-#
-# myresult = mycursor.fetchall()
-#
-# for x in myresult:
-#   print(x)
-
-
-# mydb.commit()
-# mycursor.execute("SHOW TABLES")
-#
-# for x in mycursor:
-#   print(x)
